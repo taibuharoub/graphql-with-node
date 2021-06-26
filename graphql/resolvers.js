772669c2d976fc211bdfbd6d09a759cc9f5a64f6
Taibu Haroub <haroubtaibu@gmail.com>
2021-06-26 22:18:17 +0300

    Authenication in graphql, add a login query and resolver

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const validator = require("validator");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -18,10 +19,10 @@ module.exports = {
       errors.push({ message: "password too short." });
     }
     if (errors.length > 0) {
-        const error = new Error("Invalid input.");
-        error.data = errors;
-        error.code = 422;
-        throw error;
+      const error = new Error("Invalid input.");
+      error.data = errors;
+      error.code = 422;
+      throw error;
     }
     const existingUser = await User.findOne({ email: userInput.email });
     if (existingUser) {
@@ -36,5 +37,28 @@ module.exports = {
     });
     const createdUser = await user.save();
     return { ...createdUser._doc, _id: createdUser._id.toString() };
+  },
+  login: async function ({ email, password }) {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      const error = new Error("User not found.");
+      error.code = 401;
+      throw error;
+    }
+    const isEqual = bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      const error = new Error("Password is incorrect.");
+      error.code = 401;
+      throw error;
+    }
+    const token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        email: user.email,
+      },
+      "somesecret",
+      { expiresIn: "1hr" }
+    );
+    return {token: token, userId: user._id.toString()}
   },
 };
