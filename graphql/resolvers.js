942118d9e72870df9f707655417cf9977a2ba872
Taibu Haroub <haroubtaibu@gmail.com>
2021-06-26 21:36:51 +0300

    Add input validation, error handling

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,23 +1,38 @@
 const bcrypt = require("bcryptjs");
+const validator = require("validator");
 
 const User = require("../models/user");
 
 module.exports = {
-    createUser: async function({ userInput }, req) {
-        //const email = args.user.email
-        // return User.findOne().then()
-        const existingUser = await User.findOne({email: userInput.email});
-        if (existingUser) {
-            const error = new Error("User already exists!");
-            throw error;
-        }
-        const hashedPw = await bcrypt.hash(userInput.password, 12);
-        const user = new User({
-            email: userInput.email,
-            name: userInput.name,
-            password: hashedPw
-        })
-        const createdUser = await user.save();
-        return { ...createdUser._doc, _id: createdUser._id.toString() };
+  createUser: async function ({ userInput }, req) {
+    //const email = args.user.email
+    // return User.findOne().then()
+    const errors = [];
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: "E-mail is invalid." });
     }
-}
+    if (
+      validator.isEmpty(userInput.password) ||
+      !validator.isLength(userInput.password, { min: 5 })
+    ) {
+      errors.push({ message: "password too short." });
+    }
+    if (errors.length > 0) {
+        const error = new Error("Invalid input.");
+        throw error;
+    }
+    const existingUser = await User.findOne({ email: userInput.email });
+    if (existingUser) {
+      const error = new Error("User already exists!");
+      throw error;
+    }
+    const hashedPw = await bcrypt.hash(userInput.password, 12);
+    const user = new User({
+      email: userInput.email,
+      name: userInput.name,
+      password: hashedPw,
+    });
+    const createdUser = await user.save();
+    return { ...createdUser._doc, _id: createdUser._id.toString() };
+  },
+};
